824c92cd267347e453047f5bc0bea284e6b45fe4
Patryk Krajewski <patrykraj@interia.pl>
2020-08-02 16:15:38 +0200

    Added swipe in effect

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 import Front from "./components/sections/Front";
 import Projects from "./components/sections/Projects";
 
 function App() {
+  const [imageLoaded, setImageLoaded] = useState(false);
+
   return (
     <div className="App">
       <nav>NAV</nav>
       <header>
-        <Front />
+        <Front setLoaded={setImageLoaded} />
       </header>
       <main>
         <section>
--- src/components/sections/Front.js
+++ src/components/sections/Front.js
@@ -1,14 +1,19 @@
-import React, { useState } from "react";
+import React from "react";
 
 import styled from "styled-components";
-import img from "../../images/stars.jpg";
+import img from "../../images/heheh.png";
 
-const Front = () => {
-  const [imageLoaded, setImageLoaded] = useState(false);
-
+const Front = ({ setLoaded }) => {
   return (
     <BackgroundWrapper>
-      <BackgroundImage src={img} alt="background" />
+      <BackgroundImage
+        src={img}
+        alt="background"
+        onLoad={(e) => {
+          setLoaded(true);
+          e.target.parentElement.classList.add("active");
+        }}
+      />
     </BackgroundWrapper>
   );
 };
@@ -17,9 +22,30 @@ export default Front;
 
 const BackgroundWrapper = styled.div`
   height: 100vh;
+
+  &::before {
+    content: "";
+    position: fixed;
+    display: block;
+
+    width: 100%;
+    height: 100%;
+
+    background: rgb(18, 11, 19);
+    transform: translateX(0);
+    will-change: transform;
+    transition: transform 1s cubic-bezier(0.4, 0, 1, 1);
+  }
+
+  &.active {
+    &::before {
+      transform: translateX(105%);
+    }
+  }
 `;
 
 const BackgroundImage = styled.img`
   width: 100%;
   height: 100%;
+  object-fit: contain;
 `;
